3ac99b7b0b37d4cdead18b4ae32c0f74904708c9
Nguyen Duy Y <duyy.uit@gmail.com>
2019-05-12 15:30:47 +0700

    add alert

---
+++ client-web/src/App.js
@@ -0,0 +1,31 @@
+import {React, DefaultApp, AlertManager} from 'CakeReact';
+import SectionList from "./components/SectionList";
+import CoursesView from "./components/CoursesView";
+import CourseView from "./components/CourseView";
+import AddCourseView from "./components/AddCourseView";
+import EditNodeView from "./components/EditNodeView";
+import AddNodeView from "./components/AddNodeView";
+import {Route, BrowserRouter as Router} from "react-router-dom";
+class App extends DefaultApp {
+    constructor(props) {
+        super(props);
+    }
+
+    content() {
+        return (
+            <Router>
+                <div>
+                    <div>
+                        <Route path="/" component={SectionList}/>
+                        <Route path="/courses" component={CoursesView}/>
+                        <Route path="/course/:course_id" component={CourseView}/>
+                        <Route path="/new_course" component={AddCourseView}/>
+                        <Route path="/course/:course_id/node/:node_id" component={EditNodeView}/>
+                        <Route path="/course/:course_id/new_node" component={AddNodeView}/>
+                    </div>
+                </div>
+            </Router>
+        )
+    }
+}
+export default App;
